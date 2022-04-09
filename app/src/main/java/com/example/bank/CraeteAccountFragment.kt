1df27da0e57cb5457ce4693cbe93138b615e4db4
zahhard <za.darardoust@gmail.com>
2022-04-09 19:55:35 +0430

    CraeteAccountFragment.kt updated

--- app/src/main/java/com/example/bank/CraeteAccountFragment.kt
+++ app/src/main/java/com/example/bank/CraeteAccountFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.bank.databinding.FragmentCraeteAccountBinding
 
 class CraeteAccountFragment : Fragment() {
-
+    lateinit var binding: FragmentCraeteAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,8 +16,13 @@ class CraeteAccountFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_craete_account, container, false)
+    ): View {
+       binding = FragmentCraeteAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
