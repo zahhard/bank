703c413bf1d3c62e2141c7cb6ca7659d4c1138db
zahhard <za.darardoust@gmail.com>
2022-04-09 21:33:15 +0430

    AccountRepository.kt created

---
+++ app/src/main/java/com/example/bank/AccountRepository.kt
@@ -0,0 +1,12 @@
+package com.example.bank
+
+import androidx.room.Room
+
+object AccountRepository {
+//    companion object{
+//        val dataBase = Room.databaseBuilder(
+//            applicationContext,
+//            AppDataBase::class.java, "dataBase"
+//        ).build()
+//    }
+}
