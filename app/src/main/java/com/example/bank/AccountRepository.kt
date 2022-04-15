beb78bb1ed7c41b75434b4dd57c7abd62ce64ed7
zahhard <za.darardoust@gmail.com>
2022-04-15 19:50:19 +0430

    AccountRepository.kt updated

--- app/src/main/java/com/example/bank/AccountRepository.kt
+++ app/src/main/java/com/example/bank/AccountRepository.kt
@@ -1,12 +1,30 @@
 package com.example.bank
 
-import androidx.room.Room
+import android.content.Context
 
 object AccountRepository {
-//    companion object{
-//        val dataBase = Room.databaseBuilder(
-//            applicationContext,
-//            AppDataBase::class.java, "dataBase"
-//        ).build()
-//    }
+    var db : AppDatabase? = null
+    var accountDao  : AccountDao? = null
+    var accountCounter : Long = 1000000000000000
+
+    fun initDB(context : Context){
+        db = AppDatabase.getAppDataBase(context)
+
+        accountDao = db?.questionDao()
+
+        addTestData()
+    }
+
+    private fun addTestData() {
+        accountDao?.insertAll(
+            AccountEntity(123456789, "saving", "95.5"),
+            AccountEntity(123456785, "saving", "95.5")
+        )
+    }
+
+    fun insertAccount(accountType: String, balance: String){
+        accountCounter ++
+        db?.questionDao()?.insertAll(AccountEntity(accountCounter, accountType, balance))
+    }
+
 }
