2aa1f681ae180772a2aded9c9fd4e3881b3264e4
zahhard <za.darardoust@gmail.com>
2022-04-16 22:32:23 +0430

    checkBeeFull fan added

--- app/src/main/java/com/example/bank/CraeteAccountFragment.kt
+++ app/src/main/java/com/example/bank/CraeteAccountFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.room.Room
+import com.example.bank.AppDatabase.Companion.db
 import com.example.bank.databinding.FragmentCraeteAccountBinding
 
 class CraeteAccountFragment : Fragment() {
     lateinit var binding: FragmentCraeteAccountBinding
+    val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -23,6 +27,26 @@ class CraeteAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.btnRegester.setOnClickListener {
+            checkBeFull()
+            var type = ""
+            if (binding.saving.isChecked)
+                type = binding.saving.text.toString()
+            if (binding.Gharzolhasane.isChecked)
+                type = binding.Gharzolhasane.text.toString()
+            AccountRepository.insertAccount(type , binding.myEditTextBalance.text.toString())
+        }
+    }
 
+    private fun checkBeFull() {
+        if (binding.myEditTextBirth.text.isNullOrBlank()){
+            binding.myEditTextBirth.error = ""
+        }
+        if (binding.myEditTextName.text.isNullOrBlank()){
+            binding.myEditTextName.error = ""
+        }
+        if (binding.myEditTextPost.text.isNullOrBlank()){
+            binding.myEditTextPost.error = ""
+        }
     }
 }
