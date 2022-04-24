fb04a087b192760af950d69ea57cbdc118720956
zahhard <za.darardoust@gmail.com>
2022-04-24 13:55:18 +0430

    navigationDrawer added

--- app/src/main/java/com/example/bank/MainActivity.kt
+++ app/src/main/java/com/example/bank/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.bank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.*
 import com.example.bank.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,11 +31,9 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.homeFragment, R.id.profileFragment2, R.id.loginFragment, R.id.craeteAccountFragment
+                R.id.homeFragment, R.id.profileFragment2, R.id.inputProfileFragment, R.id.craeteAccountFragment, R.id.showAccountFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -43,6 +47,28 @@ class MainActivity : AppCompatActivity() {
             }
             NavigationUI.onNavDestinationSelected(it, navController)
             drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.exit -> {
+                    val dialog = AlertDialog.Builder(this)
+                    dialog.setTitle(title)
+                    dialog.setMessage("Do you want to clear all data?")
+                    dialog.setNegativeButton("Cancel") { _, _ ->
+                    }
+                    dialog.setPositiveButton("Delete") { _, _ ->
+                        viewModel.deleteAll()
+                        this.finish()
+                    }
+                    dialog.create().show()
+                    Toast.makeText(this, "dfsdfs" , Toast.LENGTH_SHORT).show()
+                    true
+                }
+            }
+            NavigationUI.onNavDestinationSelected(it, navController)
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
